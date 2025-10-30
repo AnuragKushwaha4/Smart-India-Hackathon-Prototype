9de4afae84b29341d1552486e46f6ec8b221f9e7
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-10-30 09:47:02 +0530

    git push last

--- Agent.py
+++ Agent.py
@@ -1,106 +1,85 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["HF_API_KEY"]=os.getenv("HF_API_KEY")
+
 import streamlit as st
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
-os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
-os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["HF_API_KEY"] = st.secrets["HF_API_KEY"]
-
-
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_groq import ChatGroq
+llm= ChatGroq(model="llama-3.3-70b-versatile")
 
-st.title("RAG Document With Memory History")
-session_id = st.text_input("Session State",value="Default")
+#Tools creations:
+# 1. Builtin tools:
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchResults
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain.agents import initialize_agent,AgentType
 
-if "store" not in st.session_state:
-    st.session_state.store={}
+wikiAPI=WikipediaAPIWrapper(doc_content_chars_max=200,top_k_results=1)
+arxivAPI = ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 
-upload_file= st.file_uploader("Upload File:",type="pdf",accept_multiple_files=True)
+wikipedia = WikipediaQueryRun(api_wrapper=wikiAPI)
+arxiv = ArxivQueryRun(api_wrapper=arxivAPI)
 
-if upload_file:
-    llm= ChatGroq(model="llama-3.3-70b-versatile")
-    document =[]
-    for uploaded_files in upload_file:
-        path =f"./Resources/temp.pdf"
-        with open(path,"wb") as f:
-            f.write(uploaded_files.getvalue())
-        loader= PyPDFLoader(path)
-        docs = loader.load()
-        document.extend(docs)
+search = DuckDuckGoSearchResults(name="Search_tool")
 
+tools = [wikipedia,arxiv,search]
+
+
+st.title("AI Search Agent")
+st.sidebar.write("Upload WebLink:")
+uploaded_webpage = st.sidebar.text_input("upload Link",placeholder="https://github.com")
+if uploaded_webpage:
+    #2.Custom Wrappers for searching from specific website.
+    from langchain_community.document_loaders import WebBaseLoader
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+    from langchain_chroma import Chroma
+    from langchain.tools.retriever import create_retriever_tool
+    loader = WebBaseLoader(web_path=uploaded_webpage)
+    docs = loader.load()
     splitter =RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
-    splited_docs = splitter.split_documents(documents=document)
+    splited_docs = splitter.split_documents(documents=docs)
     embeddings =HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vector=Chroma.from_documents(documents=splited_docs,embedding=embeddings,)
-
     retriever = vector.as_retriever()
 
-    contextualize_q_system_prompt=(
-            "Given a chat history and the latest user question"
-            "which might reference context in the chat history, "
-            "formulate a standalone question which can be understood "
-            "without the chat history. Do NOT answer the question, "
-            "just reformulate it if needed and otherwise return it as is."
-        )
-    history_aware_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system",contextualize_q_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("user","{input}")
-        ]
-    )
+    retriever_tool = create_retriever_tool(
+    retriever,
+    "Web_Based_tool",
+    "Use this tool to answer questions using the content of the uploaded webpage. "
+    "If the user provides a link, prefer this tool."
+)
 
-    retriever_with_message_history = create_history_aware_retriever(llm=llm,retriever=retriever,prompt=history_aware_prompt)
-
-
-    systemPrompt=(
-                "You are an assistant for question-answering tasks. "
-                "Use the following pieces of retrieved context to answer "
-                "the question. If you don't know the answer, say that you "
-                "don't know. Use three sentences maximum and keep the "
-                "answer concise."
-                "\n\n"
-                "{context}"
-            )
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system",systemPrompt),
-            MessagesPlaceholder("chat_history"),
-            ("user","{input}")
-        ]
-    )
-    document_chain = create_stuff_documents_chain(prompt=prompt,llm=llm)
-    ragChain = create_retrieval_chain(retriever_with_message_history,document_chain)
-
-    def getsessionHistory(session_id:str)->BaseChatMessageHistory:
-        if session_id not in st.session_state.store:
-            st.session_state.store[session_id]=ChatMessageHistory()
-        return st.session_state.store[session_id]
+    tools.insert(0,retriever_tool)
     
-    Converstionalchain =RunnableWithMessageHistory(
-        ragChain,getsessionHistory,
-        input_messages_key="input",
-        output_messages_key="answer",
-        history_messages_key="chat_history"
-    )
-    input_text=st.text_input("Enter your Query..")
-    if input_text:
-        res=Converstionalchain.invoke(
-            {"input":input_text},
-            config={
-                    "configurable": {"session_id":session_id}
-                }
-        )
-        st.write(res['answer'])
+
+if "message" not in st.session_state:
+    st.session_state["message"]=[
+        {
+            "role":"Assistant","content":"Hi I am AI Agent who can search on web. How can i help you"
+        }
+    ]
+#Printing Assistant's message for users:
+for msg in st.session_state.message:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if uploaded_webpage:
+    st.session_state.message.append({"role":"user","content":uploaded_webpage})
+if prompt:=st.chat_input(placeholder="What is Generative AI?"):
+    st.session_state.message.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    Agent = initialize_agent(tools,llm,agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION)
+# Printing response
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        user_query = st.session_state.message[-1]["content"]
+        response=Agent.run(st.session_state.message,callbacks=[st_cb])
+        st.session_state.message.append({'role':'assistant', "content": response})
+        st.write(response)
+
